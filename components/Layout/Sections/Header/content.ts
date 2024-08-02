130d5a3449ac2549b9323413f46a39aeca8234c7
alizeinalzadeh <ali94zeinalzadeh@gmail.com>
2024-08-02 22:00:01 +0330

    content(global): header content

---
+++ components/Layout/Sections/Header/content.ts
@@ -0,0 +1,35 @@
+export const MOBILE_HEADER_ITEMS = [
+	{
+		id: 'support',
+		title: 'تماس با پشتیبانی',
+		href: '/',
+		icon: 'general/Phone-Telephone',
+	},
+];
+
+export const DESKTOP_HEADER_ITEMS = [
+	{
+		id: 'host',
+		title: 'میزبان شوید',
+		href: '/',
+		icon: 'general/House-Add-Seat-Residence',
+	},
+	{
+		id: 'download',
+		title: 'دانلود اپلیکیشن',
+		href: '/',
+		icon: 'general/Download-receive',
+	},
+	{
+		id: 'support',
+		title: 'پشتیبانی',
+		href: '/',
+		icon: 'general/Headset-Headphone-support',
+	},
+	{
+		id: 'login',
+		title: 'ورود / ثبت نام',
+		href: '/',
+		icon: 'general/User-Profile',
+	},
+];
