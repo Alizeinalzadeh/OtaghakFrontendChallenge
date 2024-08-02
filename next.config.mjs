d481ec503ad6d6c323531a8700490c0462467ffd
alizeinalzadeh <ali94zeinalzadeh@gmail.com>
2024-08-02 21:57:28 +0330

    config(global): improve global configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+	reactStrictMode: true,
+	compiler: {
+		styledComponents: true,
+	},
+	i18n: {
+		locales: ['fa'],
+		defaultLocale: 'fa',
+	},
+	images: {
+		unoptimized: true,
+		domains: ['https://cdn.otaghak.com/otg-images-new/X500/'],
+	},
 };
 
 export default nextConfig;
